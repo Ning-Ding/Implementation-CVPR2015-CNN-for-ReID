12ffb9b23462c1db699c96d94279a7809ea3314c
dingningbuaa <dingning.buaa@gmail.com>
2016-11-05 22:41:28 +0800

    Update make_hdf5_for_market1501.py

--- make_hdf5_for_market1501.py
+++ make_hdf5_for_market1501.py
@@ -56,9 +56,9 @@ def get_image_path_list(train_or_test = 'train'):
     ---------------------------------------------------------------------------
     '''
     if train_or_test == 'train':
-        folder_path = '~/dataset/market1501/boundingboxtrain'
+        folder_path = '~//dataset//market1501//boundingboxtrain'
     else:
-        folder_path = '~/dataset/market1501/boundingboxtest'
+        folder_path = '~//dataset//market1501//boundingboxtest'
     assert os.path.exists(folder_path)
     assert os.path.isdir(folder_path)
     print 'already get all the image path.'
