596fd47260d985df02f5bdd08598a48fb2d2802c
dingningbuaa <dingning.buaa@gmail.com>
2016-11-08 21:23:41 +0800

    Update model_for_market1501.py

--- market1501/model_for_market1501.py
+++ market1501/model_for_market1501.py
@@ -361,7 +361,7 @@ def random_select(user_name = 'ubuntu', num = 10):
 
 
 def cmc(model, random_translate = False):
-    a,b = random_select_100(num = 751)
+    a,b = random_select_100(num = 750)
     if random_translate:
         Data_Generator = ImageDataGenerator_for_multiinput(width_shift_range=0.05,height_shift_range=0.05)
         a = Data_Generator.agumentation(a)
