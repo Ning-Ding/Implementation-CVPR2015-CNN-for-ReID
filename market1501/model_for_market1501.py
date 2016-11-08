15be1cd0b8aa06f7fb9d315b5fdc55529b373da0
dingningbuaa <dingning.buaa@gmail.com>
2016-11-08 15:36:25 +0800

    Update model_for_market1501.py

--- market1501/model_for_market1501.py
+++ market1501/model_for_market1501.py
@@ -346,6 +346,28 @@ def random_select(user_name = 'ubuntu', num = 10):
         
     return np.array(A)/255.,np.array(B)/255.
 
+
+def cmc_curve(model, camera1, camera2, rank_max=50):
+    num = camera1.shape[0]    
+    rank = []
+    score = []    
+    camera_batch1 = np.zeros(camera1.shape)
+    for i in range(num):
+        for j in range(num):
+            camera_batch1[j] = camera1[i]
+        similarity_batch = model.predict_on_batch([camera_batch1, camera2])
+        sim_trans = similarity_batch.transpose()
+        similarity_rate_sorted = np.argsort(sim_trans[0])
+        for k in range(num):
+            if similarity_rate_sorted[k] == i:
+                rank.append(k+1)
+                break
+    rank_val = 0
+    for i in range(rank_max):
+        rank_val = rank_val + len([j for j in rank if i == j-1])        
+        score.append(rank_val / float(num))
+    return np.array(score)        
+
 if __name__ == '__main__':
     print 'default dim order is:',K.image_dim_ordering()
     user_name = raw_input('please input your system user name:')
