8e5c9f0813918a29e4d512f0cd7c472be8839014
Ning Ding <dingning.buaa@gmail.com>
2017-05-18 20:26:28 +0800

    Update model_definition.py

--- CUHK03/model_definition.py
+++ CUHK03/model_definition.py
@@ -15,7 +15,7 @@ from __future__ import print_function
 
 import tensorflow as tf
 from tensorflow.contrib.keras.python.keras.layers.convolutional import Conv2D,UpSampling2D
-from tensorflow.contrib.keras.python.keras.layers.pooling import MaxPooling2D
+from tensorflow.contrib.keras.python.keras.layers.pooling import MaxPool2D
 from tensorflow.contrib.keras.python.keras.regularizers import l2
 from tensorflow.contrib.keras.python.keras.layers.core import Lambda,Flatten,Dense
 from tensorflow.contrib.keras.python.keras.engine.topology import Input
@@ -39,20 +39,22 @@ def tf_model_definition(weight_decay=0.0005):
     def upsample_neighbor_shape(input_shape):
         return (input_shape[0],input_shape[1] * 5,input_shape[2] * 5,input_shape[3])
     
+    max_pooling = MaxPool2D(2)
+    
     x1_input = Input(shape=(160,60,3))
     x2_input = Input(shape=(160,60,3))
     
     share_conv_1 = Conv2D(20, 5, kernel_regularizer=l2(weight_decay), activation="relu")
     x1 = share_conv_1(x1_input)
     x2 = share_conv_1(x2_input)
-    x1 = MaxPooling2D(x1)
-    x2 = MaxPooling2D(x2)
+    x1 = max_pooling(x1)
+    x2 = max_pooling(x2)
     
     share_conv_2 = Conv2D(25, 5, kernel_regularizer=l2(weight_decay), activation="relu")
     x1 = share_conv_2(x1)
     x2 = share_conv_2(x2)
-    x1 = MaxPooling2D(x1)
-    x2 = MaxPooling2D(x2)
+    x1 = max_pooling(x1)
+    x2 = max_pooling(x2)
     
     upsample_same = UpSampling2D(size=(5, 5))
     x1_up = upsample_same(x1)
@@ -73,8 +75,8 @@ def tf_model_definition(weight_decay=0.0005):
     conv_4_2 = Conv2D(25, 3, kernel_regularizer=l2(weight_decay), activation="relu")
     x1 = conv_4_1(x1)
     x2 = conv_4_2(x2)
-    x1 = MaxPooling2D(x1)
-    x2 = MaxPooling2D(x2)
+    x1 = max_pooling(x1)
+    x2 = max_pooling(x2)
     
     y = tf.concat([x1, x2], -1)
     y = Flatten(y)
