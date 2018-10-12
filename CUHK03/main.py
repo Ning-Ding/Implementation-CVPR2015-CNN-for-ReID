4356fb7cd2e69e786461ce95c72bfc20c0ebb0d6
Ning Ding <dingning.buaa@gmail.com>
2018-10-12 17:57:28 +0800

    Update main.py

--- CUHK03/main.py
+++ CUHK03/main.py
@@ -6,7 +6,8 @@
 # Written by Ning Ding
 # --------------------------------------------------------
 
-import os,sys
+import os
+import sys
 import h5py
 import itertools
 import numpy as np
@@ -15,43 +16,72 @@ from PIL import Image
 from tensorflow import keras
 from tensorflow.keras import backend as K
 from tensorflow.keras.regularizers import l2
-from config import cfg
+from easydict import EasyDict
 
-os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]=str(cfg.TRAIN.GPU_INDEX)
+__C = EasyDict()
+__C.DATA = EasyDict()
+__C.DATA.ORIGINAL_FILE = "cuhk-03.mat"
+__C.DATA.CREATED_FILE = "cuhk-03.hdf5"
+__C.DATA.INDEX_FILE = "cuhk-03-index.hdf5"
+__C.DATA.IMAGE_SIZE = (60, 160)
+__C.DATA.ARRAY_SIZE = (160, 60)
+__C.DATA.PATTERN = EasyDict()
+__C.DATA.PATTERN.TRAIN = [1, 0, 0]
+__C.DATA.PATTERN.VALID = [1, 0]
+__C.TRAIN = EasyDict()
+__C.TRAIN.BATCHSIZE = 150
+__C.TRAIN.STEPS = 2100
+__C.TRAIN.WEIGHT_DECAY = 0.00025
+__C.TRAIN.GPU_INDEX = 0
+__C.VALID = EasyDict()
+__C.VALID.STEPS = 1
+
+cfg = __C
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = str(cfg.TRAIN.GPU_INDEX)
+
 
 # -------------------------------------------------------
 # -------------------------------------------------------
 # model section
-#@export_estimator
+# @export_estimator
 def generate_model(weight_decay=cfg.TRAIN.WEIGHT_DECAY):
 
-    #Input Pair of images
-    x1_input = keras.layers.Input(shape=(*cfg.DATA.ARRAY_SIZE,3), name="x1_input")
-    x2_input = keras.layers.Input(shape=(*cfg.DATA.ARRAY_SIZE,3), name="x2_input")
-    
-    #Tied Convolution with max pooling
-    share_conv_1 = keras.layers.Conv2D(20, 5, activation="relu", kernel_regularizer=l2(weight_decay), name="share_conv_1")
+    # Input Pair of images
+    x1_input = keras.layers.Input(shape=(*cfg.DATA.ARRAY_SIZE, 3),
+                                  name="x1_input")
+
+    x2_input = keras.layers.Input(shape=(*cfg.DATA.ARRAY_SIZE, 3),
+                                  name="x2_input")
+
+    # Tied Convolution with max pooling
+    share_conv_1 = keras.layers.Conv2D(20, 5,
+                                       activation="relu",
+                                       kernel_regularizer=l2(weight_decay),
+                                       name="share_conv_1")
 
     x1 = share_conv_1(x1_input)
     x2 = share_conv_1(x2_input)
     x1 = keras.layers.MaxPooling2D(pool_size=(2, 2))(x1)
     x2 = keras.layers.MaxPooling2D(pool_size=(2, 2))(x2)
-    
-    share_conv_2 = keras.layers.Conv2D(25, 5, activation="relu", kernel_regularizer=l2(weight_decay), name="share_conv_2")
+
+    share_conv_2 = keras.layers.Conv2D(25, 5,
+                                       activation="relu",
+                                       kernel_regularizer=l2(weight_decay),
+                                       name="share_conv_2")
 
     x1 = share_conv_2(x1)
     x2 = share_conv_2(x2)
     x1 = keras.layers.MaxPooling2D(pool_size=(2, 2))(x1)
     x2 = keras.layers.MaxPooling2D(pool_size=(2, 2))(x2)
 
-
-    #Cross-Input Neighborhood Differences 
+    # Cross-Input Neighborhood Differences
     x1_up = keras.layers.UpSampling2D(size=(5, 5))(x1)
-    x2_up = keras.layers.UpSampling2D(size=(5, 5))(x2) 
+    x2_up = keras.layers.UpSampling2D(size=(5, 5))(x2)
 
-    x1_nn = keras.layers.Lambda(_upsample_neighbor_function) (x1)
-    x2_nn = keras.layers.Lambda(_upsample_neighbor_function) (x2)
+    x1_nn = keras.layers.Lambda(_upsample_neighbor_function)(x1)
+    x2_nn = keras.layers.Lambda(_upsample_neighbor_function)(x2)
 
     x1_nn = keras.layers.Lambda(lambda x: -x)(x1_nn)
     x2_nn = keras.layers.Lambda(lambda x: -x)(x2_nn)
@@ -59,51 +89,69 @@ def generate_model(weight_decay=cfg.TRAIN.WEIGHT_DECAY):
     x1 = keras.layers.Add()([x1_up, x2_nn])
     x2 = keras.layers.Add()([x2_up, x1_nn])
 
+    # Patch Summary Features
+    conv_3_1 = keras.layers.Conv2D(25, 5, strides=(5, 5),
+                                   activation="relu",
+                                   kernel_regularizer=l2(weight_decay),
+                                   name="conv_3_1")
 
-    #Patch Summary Features
-    conv_3_1 = keras.layers.Conv2D(25, 5, strides=(5, 5), activation="relu", kernel_regularizer=l2(weight_decay), name="conv_3_1")
-    conv_3_2 = keras.layers.Conv2D(25, 5, strides=(5, 5), activation="relu", kernel_regularizer=l2(weight_decay), name="conv_3_2")
+    conv_3_2 = keras.layers.Conv2D(25, 5, strides=(5, 5),
+                                   activation="relu",
+                                   kernel_regularizer=l2(weight_decay),
+                                   name="conv_3_2")
     x1 = conv_3_1(x1)
     x2 = conv_3_2(x2)
 
-    #Across-Patch Features
-    conv_4_1 = keras.layers.Conv2D(25, 3, activation="relu", kernel_regularizer=l2(weight_decay), name="conv_4_1")
-    conv_4_2 = keras.layers.Conv2D(25, 3, activation="relu", kernel_regularizer=l2(weight_decay), name="conv_4_2")
+    # Across-Patch Features
+    conv_4_1 = keras.layers.Conv2D(25, 3,
+                                   activation="relu",
+                                   kernel_regularizer=l2(weight_decay),
+                                   name="conv_4_1")
+
+    conv_4_2 = keras.layers.Conv2D(25, 3,
+                                   activation="relu",
+                                   kernel_regularizer=l2(weight_decay),
+                                   name="conv_4_2")
     x1 = conv_4_1(x1)
     x2 = conv_4_2(x2)
     x1 = keras.layers.MaxPooling2D(pool_size=(2, 2), padding='same')(x1)
     x2 = keras.layers.MaxPooling2D(pool_size=(2, 2), padding='same')(x2)
 
-    #Higher-Order Relationships
+    # Higher-Order Relationships
     y = keras.layers.Concatenate()([x1, x2])
     y = keras.layers.Flatten()(y)
-    y = keras.layers.Dense(500, kernel_regularizer=l2(weight_decay), activation='relu')(y)
-    y = keras.layers.Dense(2, kernel_regularizer=l2(weight_decay), activation='softmax')(y)
-    
+    y = keras.layers.Dense(500,
+                           kernel_regularizer=l2(weight_decay),
+                           activation='relu')(y)
+
+    y = keras.layers.Dense(2,
+                           kernel_regularizer=l2(weight_decay),
+                           activation='softmax')(y)
+
     model = keras.Model(inputs=[x1_input, x2_input], outputs=[y])
     model.summary()
 
     model = _compile_model(model)
-    
+
     return model
 
 
 def _upsample_neighbor_function(input_x):
-    input_x_pad = K.spatial_2d_padding(input_x, padding=((2,2),(2,2)))
+    input_x_pad = K.spatial_2d_padding(input_x, padding=((2, 2), (2, 2)))
     x_length = K.int_shape(input_x)[1]
     y_length = K.int_shape(input_x)[2]
     output_x_list = []
     output_y_list = []
     for i_x in range(2, x_length + 2):
         for i_y in range(2, y_length + 2):
-            output_y_list.append(input_x_pad[:,i_x-2:i_x+3,i_y-2:i_y+3,:])
+            output_y_list.append(input_x_pad[:, i_x-2:i_x+3, i_y-2:i_y+3, :])
         output_x_list.append(K.concatenate(output_y_list, axis=2))
         output_y_list = []
     return K.concatenate(output_x_list, axis=1)
 
 
 def _compile_model(model):
-    sgd = tf.keras.optimizers.SGD(lr=0.01,momentum=0.9)
+    sgd = tf.keras.optimizers.SGD(lr=0.01, momentum=0.9)
     model.compile(optimizer=sgd,
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
@@ -119,37 +167,38 @@ def get_data_generator(mode='train', pattern=cfg.DATA.PATTERN.TRAIN):
         for pos_or_neg in itertools.cycle(pattern):
             if pos_or_neg:
                 image_x, image_y = _generate_positive_pair(mode)
-                yield ((image_x,image_y),(1,0))
+                yield ((image_x, image_y), (1, 0))
             else:
                 image_x, image_y = _generate_negative_pair(mode)
-                yield ((image_x,image_y),(0,1))
+                yield ((image_x, image_y), (0, 1))
 
     return gen_data
 
 
 def _generate_positive_pair(mode='train'):
-    with h5py.File(cfg.DATA.CREATED_FILE,'r') as f:
+    with h5py.File(cfg.DATA.CREATED_FILE, 'r') as f:
         index_array = _get_index_array(mode)
         i = np.random.choice(index_array)
-        x,y = np.random.choice(f[str(i)].shape[0], 2, replace=False)
+        x, y = np.random.choice(f[str(i)].shape[0], 2, replace=False)
         image_x = _image_augmentation(f[str(i)][x])
         image_y = _image_augmentation(f[str(i)][y])
-        return image_x,image_y
+        return image_x, image_y
 
 
 def _generate_negative_pair(mode='train'):
-    with h5py.File(cfg.DATA.CREATED_FILE,'r') as f:
-        index_array=_get_index_array(mode)
-        i,j = np.random.choice(index_array,2, replace=False)
+    with h5py.File(cfg.DATA.CREATED_FILE, 'r') as f:
+        index_array = _get_index_array(mode)
+        i, j = np.random.choice(index_array, 2, replace=False)
         x = np.random.choice(f[str(i)].shape[0], replace=False)
         y = np.random.choice(f[str(j)].shape[0], replace=False)
         image_x = f[str(i)][x]
         image_y = f[str(j)][y]
-        return image_x,image_y
+        return image_x, image_y
+
 
 def _get_index_array(mode='train'):
 
-    with h5py.File(cfg.DATA.INDEX_FILE,'r') as f:
+    with h5py.File(cfg.DATA.INDEX_FILE, 'r') as f:
         index_array = f[mode][:]
 
     return index_array
@@ -157,8 +206,24 @@ def _get_index_array(mode='train'):
 
 # soon
 def _image_augmentation(image):
+    x_padding = int(np.round(image.shape[0] * 0.05))
+    y_padding = int(np.round(image.shape[1] * 0.05))
+    padding_shape = x_padding * 2, y_padding * 2
+    image_shape = np.array(image.shape[:2]) + np.array(padding_shape)
+    image_padding = np.zeros((image_shape[0]+padding_shape[0],
+                              image_shape[1]+padding_shape[1],
+                              3))
+    image_padding[x_padding:x_padding+image.shape[0],
+                  y_padding:y_padding+image.shape[1],
+                  :] = image
 
-    return image
+    x_translation = np.random.choice(x_padding * 2)
+    y_translation = np.random.choice(y_padding * 2)
+    new_image = image_padding[x_translation:x_translation+image.shape[0],
+                              y_translation:y_translation+image.shape[1],
+                              :]
+
+    return new_image
 
 
 # -------------------------------------------------------
@@ -166,13 +231,13 @@ def _image_augmentation(image):
 
 def generate_data():
 
-    with h5py.File(cfg.DATA.ORIGINAL_FILE,'r') as fr, h5py.File(cfg.DATA.CREATED_FILE,'w') as fw:
+    with h5py.File(cfg.DATA.ORIGINAL_FILE, 'r') as fr, h5py.File(cfg.DATA.CREATED_FILE, 'w') as fw:
         """f[f[f['labeled'][0][i]][j][k]] get a HDF5 Dataset
         i: index from 0-4 denoted different cameras.
         j: index from 0-9 denoted different photos of identities.
         k: index denoted different identities captured by the camera.
-        
-        notes: 
+
+        notes:
         1. not all j range of 0-9, it may only range 0-5.
         2. the numpy arrays are with different size.
         3. the numpy array's dimenstion: channels, width, height.
@@ -180,15 +245,16 @@ def generate_data():
         5. the five camera each contain: 843, 440, 77, 58, 49 identities.
         """
         for i in range(3):
-            for k in range(_get_identity_size(fr,i)):
-                print("Now generated {} identities.".format(_compute_index(i,k)+1))
+            for k in range(_get_identity_size(fr, i)):
+                print("Now generated {} identities.".format(_compute_index(i, k) + 1))
                 temp = []
                 for j in range(10):
                     array = _get_array(fr, i, j, k)
-                    if array is not None: 
+                    if array is not None:
                         temp.append(array)
 
-                fw.create_dataset(str(_compute_index(i,k)), data=np.array(temp))
+                fw.create_dataset(str(_compute_index(i, k)), data=np.array(temp))
+
         print("HDF5 Dataset Already Created.")
 
 
@@ -199,15 +265,15 @@ def random_split_dataset():
     index_valid = np.random.choice(res, 100, replace=False)
     index_train = np.array(list((set(res) - set(index_valid))))
 
-    with h5py.File(cfg.DATA.INDEX_FILE,'w') as f:
+    with h5py.File(cfg.DATA.INDEX_FILE, 'w') as f:
         f.create_dataset('train', data=index_train)
         f.create_dataset('valid', data=index_valid)
         f.create_dataset('test', data=index_test)
 
     print("Index Dataset Already Created.")
-        
 
-def image_preprocessing(transpose=(2,1,0), image_size=(60,160)):
+
+def image_preprocessing(transpose=(2, 1, 0), image_size=(60, 160)):
     def image_preprocessing_decorator(fn):
         def updated_fn(*args, **kw):
             result = fn(*args, **kw)
@@ -216,8 +282,8 @@ def image_preprocessing(transpose=(2,1,0), image_size=(60,160)):
                 image = Image.fromarray(result[:].transpose(transpose))
                 image = image.resize(image_size)
 
-                #default return (160,60,3) 0-1 dtype=float64 numpy array.
-                return np.array(image) / 255.   
+                # default return (160,60,3) 0-1 dtype=float64 numpy array.
+                return np.array(image) / 255.
             else:
                 return None
         return updated_fn
@@ -233,7 +299,7 @@ def _get_identity_size(File, camera):
     return File[File['labeled'][0][camera]][0].size
 
 
-def _compute_index(i,k):
+def _compute_index(i, k):
     if i == 0:
         return k
     elif i == 1:
@@ -249,14 +315,14 @@ def train_input_fn():
     dataset = tf.data.Dataset.from_generator(
                      get_data_generator(),
                      ((tf.float32, tf.float32), tf.int8),
-                     ((tf.TensorShape([*cfg.DATA.ARRAY_SIZE,3]),
-                       tf.TensorShape([*cfg.DATA.ARRAY_SIZE,3])), 
-                       tf.TensorShape(None))
+                     ((tf.TensorShape([*cfg.DATA.ARRAY_SIZE, 3]),
+                       tf.TensorShape([*cfg.DATA.ARRAY_SIZE, 3])),
+                      tf.TensorShape(None))
                     )
 
     dataset = dataset.batch(batch_size=cfg.TRAIN.BATCHSIZE)
     dataset = dataset.prefetch(buffer_size=150)
-    
+
     return dataset
 
 
@@ -264,9 +330,9 @@ def valid_input_fn():
     dataset = tf.data.Dataset.from_generator(
         get_data_generator(mode='valid', pattern=cfg.DATA.PATTERN.VALID),
         ((tf.float32, tf.float32), tf.int8),
-        ((tf.TensorShape([*cfg.DATA.ARRAY_SIZE,3]),
-          tf.TensorShape([*cfg.DATA.ARRAY_SIZE,3])), 
-          tf.TensorShape(None)))
+        ((tf.TensorShape([*cfg.DATA.ARRAY_SIZE, 3]),
+          tf.TensorShape([*cfg.DATA.ARRAY_SIZE, 3])),
+         tf.TensorShape(None)))
 
     dataset = dataset.batch(100)
     dataset = dataset.prefetch(buffer_size=100)
@@ -291,13 +357,13 @@ def prepare_keras_callback():
     callbacks.append(callback_tensorboard)
 
     callback_mcp = tf.keras.callbacks.ModelCheckpoint(
-                filepath='weights.{epoch:06d}-{val_loss:.4f}.checkpoint', 
-                monitor='val_loss', 
-                verbose=1, 
-                mode='auto', 
+                filepath='weights.{epoch:06d}-{val_loss:.4f}.checkpoint',
+                monitor='val_loss',
+                verbose=1,
+                mode='auto',
                 period=1000)
     callbacks.append(callback_mcp)
-    
+
     return callbacks
 
 
@@ -335,6 +401,7 @@ def _check_created_dataset():
         else:
             sys.exit()
 
+
 def _check_original_dataset():
     if not os.path.exists(cfg.DATA.ORIGINAL_FILE):
         print("Can't find the original dataset file.")
@@ -342,12 +409,13 @@ def _check_original_dataset():
         print("Find it, and come back lator.")
         sys.exit()
 
+
 def _check_index_dataset():
     if not os.path.exists(cfg.DATA.INDEX_FILE):
         print("File is not Exists.")
         print("Creating new index file.")
         random_split_dataset()
-    
+
 
 if __name__ == '__main__':
 
@@ -357,24 +425,14 @@ if __name__ == '__main__':
     train_dataset = train_input_fn()
     valid_dataset = valid_input_fn()
     callbacks = prepare_keras_callback()
-    
 
-
-    history = model.fit(train_dataset, 
+    history = model.fit(train_dataset,
                         validation_data=valid_dataset,
                         callbacks=callbacks,
-                        verbose=1, 
-                        steps_per_epoch=100, 
+                        verbose=1,
+                        steps_per_epoch=100,
                         epochs=cfg.TRAIN.STEPS,
                         validation_steps=1)
 
     # estimator = generate_model()
     # estimator.train(steps=cfg.TRAIN.STEPS, input_fn=train_input_fn)
-
-
-
-
-
-
-
-
